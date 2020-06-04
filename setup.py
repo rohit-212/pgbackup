d2cdd8a539452d2701cc37cbfed84e0c8750b711
rohit-212 <41030243+rohit-212@users.noreply.github.com>
2020-06-04 14:58:22 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+        name='pgbackup',
+        version='0.1.0',
+        author='Wyatt Newcomb',
+        author_email='jwn150@gmail.com',
+        description='A utility key for backing up PostgreSQL databases.',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/WyNewcomb/pgbackup',
+        packages=find_packages('src'),
+        package_dir={'':'src'},
+        install_requires=['boto3'],
+        python_requires='>=3.6',
+        entry_points={
+            'console_scripts': [
+                'pgbackup=pgbackup.cli:main'
+                ],
+            }
+        )
